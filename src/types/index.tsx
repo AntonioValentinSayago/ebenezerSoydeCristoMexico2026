32d31938c4bd73057cbc634a38cba02102b1d1ae
AntonioValentinSayago <cesar_anto30@hotmail.com>
2026-03-20 23:35:51 -0600

    add(*): Folder for implementing component typing

---
+++ src/types/index.tsx
@@ -0,0 +1,3 @@
+export type UserRole = "" | "pastor" | "siervo";
+export type ShirtSize = "" | "CH" | "M" | "G" | "XG" | "2XG";
+export type PaymentMethod = "" | "stripe" | "mercado-pago" | "conekta" | "transferencia";
